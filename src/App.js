536d10cb72df45deba38f3e6749e0992a4287235
SRYVPRASAD <sryvprasad@gmail.com>
2021-11-21 22:09:11 +1100

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import Splash from "./Components/SplashScreen";
+
+const LightTheme = {
+  pageBackground: "white",
+  titleColor: "#dc4345",
+  tagLineColor: "#a234a",
+};
+const DarkTheme = {
+  pageBackground: "#282c36",
+  titleColor: "lightpink",
+  tagLineColor: "lavender",
+};
+const themes = { light: LightTheme, dark: DarkTheme };
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <Splash theme={theme} setTheme={setTheme}></Splash>
+    </ThemeProvider>
   );
 }
 
